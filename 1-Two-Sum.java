83f3bc561cd2b02c224dde7a9acf07d7ce83c316
Debabrata Ghosh <121720800+SUPERREALCODER@users.noreply.github.com>
2024-11-05 00:21:20 +0530

    [1] [Time Beats: 49.50%] [Memory Beats: 93.15%] - LeetPush

---
+++ 1-Two-Sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] m = new int[2];
+        int i,j;
+        for(i=0;i<nums.length;i++)
+        {
+            for(j=i+1;j<nums.length;j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    m[0]=i;
+                    m[1]=j;
+                    break;
+                }
+                
+
+            }
+        }
+        return m;
+        
+    }
+
+ 
+}
