c1c09c68b3cc78c588cc2855f22aa5cafdabb934
Debabrata Ghosh <121720800+SUPERREALCODER@users.noreply.github.com>
2024-11-06 00:42:32 +0530

    [9] [Time Beats: 100.00%] [Memory Beats: 87.76%] - LeetPush

---
+++ 9-Palindrome-Number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int i=x;
+        int d,r=0;
+        while(i!=0)
+        {
+            d=i%10;
+            r=r*10+d;
+            i=i/10;
+
+        }
+        if(x>=0){
+        if(x == r)
+        return true;
+        else 
+        return false;
+        }
+        else 
+        return false;
+        
+    }
+}
